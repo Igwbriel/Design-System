faf466a3a195a48eff44d8c661a37eafcd91bd95
Gabriel Dias <GabrielDias7820@gmail.com>
2024-01-15 22:20:24 -0300

    Documentação 'space' que servirá modelo para os demais tokens

--- packages/tokens/src/colors.ts
+++ packages/tokens/src/colors.ts
@@ -1,10 +1,10 @@
 export const colors = {
-  white: '#FFF',
-  black: '#000',
+  white: '#FFFFFF',
+  black: '#000000',
 
   gray100: '#E1E1E6',
   gray200: '#A9A9B2',
-  gray400: '#7C7CBA',
+  gray400: '#7C7C8A',
   gray500: '#505059',
   gray600: '#323238',
   gray700: '#29292E',
--- packages/tokens/src/space.ts
+++ packages/tokens/src/space.ts
@@ -14,5 +14,4 @@ export const space = {
   40: '10rem',
   64: '16rem',
   80: '20rem',
-  160: '40rem',
 }
