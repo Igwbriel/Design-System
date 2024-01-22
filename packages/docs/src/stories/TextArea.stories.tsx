c0d5bbfbf3bad71048106c56ec344ebd8297178a
Gabriel Dias <GabrielDias7820@gmail.com>
2024-01-21 21:01:01 -0300

    Componente TextArea

---
+++ packages/docs/src/stories/TextArea.stories.tsx
@@ -0,0 +1,34 @@
+import { StoryObj, Meta } from '@storybook/react'
+import { Box, Text, TextArea, TextAreaProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Form/Text Area',
+  component: TextArea,
+  args: {},
+  decorators: [
+    (Story) => {
+      return (
+        <Box
+          as="label"
+          css={{ display: 'flex', flexDirection: 'column', gap: '$2' }}
+        >
+          <Text size="sm">Observations</Text>
+          {Story()}
+        </Box>
+      )
+    },
+  ],
+  tags: ['autodocs'],
+} as Meta<TextAreaProps>
+
+export const Primary: StoryObj<TextAreaProps> = {
+  args: {
+    placeholder: 'add any observations...',
+  },
+}
+
+export const Disabled: StoryObj<TextAreaProps> = {
+  args: {
+    disabled: true,
+  },
+}
--- packages/react/dist/index.js
+++ packages/react/dist/index.js
@@ -64,6 +64,7 @@ __export(src_exports, {
   Button: () => Button,
   Heading: () => Heading,
   Text: () => Text,
+  TextArea: () => TextArea,
   TextInput: () => TextInput
 });
 module.exports = __toCommonJS(src_exports);
@@ -389,6 +390,32 @@ function TextInput(_a) {
     /* @__PURE__ */ (0, import_jsx_runtime2.jsx)(Input, __spreadValues({}, props))
   ] });
 }
+
+// src/components/TextArea.tsx
+var TextArea = styled("textarea", {
+  backgroundColor: "$gray900",
+  padding: "$3 $4",
+  borderRadius: "$sm",
+  boxSizing: "border-box",
+  border: "2px solid $gray900",
+  fontFamily: "$default",
+  fontSize: "$sm",
+  color: "$white",
+  fontWeight: "$regular",
+  resize: "vertical",
+  minHeight: 80,
+  "&:focus": {
+    outline: 0,
+    borderColor: "$ignite300"
+  },
+  "&:disabled": {
+    outline: 0.5,
+    cursor: "not-allowed"
+  },
+  "&:placeholder": {
+    color: "$gray400"
+  }
+});
 // Annotate the CommonJS export names for ESM import in node:
 0 && (module.exports = {
   Avatar,
@@ -396,5 +423,6 @@ function TextInput(_a) {
   Button,
   Heading,
   Text,
+  TextArea,
   TextInput
 });
